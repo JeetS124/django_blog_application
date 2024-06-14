4ceb6d8ac52360bc45a56968cb633fb77bd08102
Jitendra Sharma <js.sharma124@gmail.com>
2024-06-14 15:39:39 +0530

    update build config

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,14 @@
-pip install -r requirements.txt
-python manage.py collectstatic
+#!/bin/bash
+
+# Path to Pip executable
+PIP_PATH="C:\Program Files\Python312\Scripts\pip.exe"
+
+# Path to Python executable
+PYTHON_PATH="C:/Program Files/Python312/python.exe"
+
+# Install dependencies
+"$PIP_PATH" install -r requirements.txt
+
+# Run build commands
+"$PYTHON_PATH" manage.py migrate
+"$PYTHON_PATH" manage.py collectstatic --no-input
