4c875befbc5ddf058700e4aceaf532f2ae41705a
Jitendra Sharma <128837537+JeetS124@users.noreply.github.com>
2024-06-14 15:19:24 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,2 @@
-pip install -r requirements.txt
-python manage.py collectstatic
+C:\Program Files\Python312\Scripts\pip install -r requirements.txt
+C:\Program Files\Python312 manage.py collectstatic
