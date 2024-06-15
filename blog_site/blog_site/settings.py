adebd1e9af041ccc4c237f0a5137288d60e53bca
Jitendra Sharma <128837537+JeetS124@users.noreply.github.com>
2024-06-15 13:30:35 +0530

    Update settings.py

--- blog_site/blog_site/settings.py
+++ blog_site/blog_site/settings.py
@@ -122,7 +122,7 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build")
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
