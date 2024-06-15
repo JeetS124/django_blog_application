bf0d9eb4ee755767b613be1fbce4a300f9e8f214
Jitendra Sharma <128837537+JeetS124@users.noreply.github.com>
2024-06-15 13:15:13 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,9 @@
 echo "BUILD START"
  python3.12 -m pip install -r requirements.txt
 
+ echo "CHANGE DIRECTORY"
+cd blog_site
+
  echo "Make Migration..."
 python3.12 manage.py makemigrations --noinput
 python3.12 manage.py migrate --noinput
