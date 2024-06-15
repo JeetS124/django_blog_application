111d458890e6a5de8efcad0b54d3001015e1727b
Jitendra Sharma <js.sharma124@gmail.com>
2024-06-15 11:53:22 +0530

    update build config file

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,9 @@
 echo "BUILD START"
  python3.12 -m pip install -r requirements.txt
+
+ echo "Make Migration..."
+python3.12 manage.py makemigrations --noinput
+python3.12 manage.py migrate --noinput
+
  python3.12 manage.py collectstatic --noinput --clear
  echo "BUILD END"
